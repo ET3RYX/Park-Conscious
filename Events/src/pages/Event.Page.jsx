2bbaba3e9dabee665af8f9e97f81bd50980c8e78
Piyush Kumar <kumarpiyush2k6@gmail.com>
2026-03-31 13:06:49 +0530

    fix(Events): Fix all relative import paths in Event.Page.jsx breaking Vercel build

--- Events/src/pages/Event.Page.jsx
+++ Events/src/pages/Event.Page.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { backendAxios } from "../../axios";
-import DefaultlayoutHoc from "../../layout/Default.layout";
-import EventHero from "../../components/EventHero/EventHero.Component";
+import { backendAxios } from "../axios";
+import DefaultlayoutHoc from "../layout/Default.layout";
+import EventHero from "../components/EventHero/EventHero.Component";
 import { Info, MapPin, Share2, ShieldCheck, Ticket } from "lucide-react";
 
 const EventPage = () => {
