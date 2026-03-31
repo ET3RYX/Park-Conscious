3d4ac589f321749a8a44bc1c481c08c0f4b43f60
Piyush Kumar <kumarpiyush2k6@gmail.com>
2026-04-01 00:53:54 +0530

    debug(api): isolation phase 5 - zero model auth test

--- api/handlers/auth.handler.js
+++ api/handlers/auth.handler.js
@@ -3,30 +3,30 @@ import { OAuth2Client } from 'google-auth-library';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import axios from 'axios';
-*/
 import { User, Owner } from '../lib/models.js';
+*/
 import { sendJSON, sendError } from '../utils/responses.js';
 
 export const handleUserSignup = async (req, res, body) => {
-    return sendJSON(res, 200, { status: 'AUTH_CLEAN_ROOM_LIVE', note: 'Imports currently disabled for isolation' });
+    return sendJSON(res, 200, { status: 'AUTH_ZERO_MODEL_LIVE', note: 'All imports (including models) disabled' });
 };
 
 export const handleUserLogin = async (req, res, body) => {
-    return sendJSON(res, 200, { status: 'AUTH_CLEAN_ROOM_LIVE' });
+    return sendJSON(res, 200, { status: 'AUTH_ZERO_MODEL_LIVE' });
 };
 
 export const handleGoogleLogin = async (req, res, body) => {
-    return sendJSON(res, 200, { status: 'AUTH_CLEAN_ROOM_LIVE' });
+    return sendJSON(res, 200, { status: 'AUTH_ZERO_MODEL_LIVE' });
 };
 
 export const handleOwnerSignup = async (req, res, body) => {
-    return sendJSON(res, 200, { status: 'AUTH_CLEAN_ROOM_LIVE' });
+    return sendJSON(res, 200, { status: 'AUTH_ZERO_MODEL_LIVE' });
 };
 
 export const handleOwnerLogin = async (req, res, body) => {
-    return sendJSON(res, 200, { status: 'AUTH_CLEAN_ROOM_LIVE' });
+    return sendJSON(res, 200, { status: 'AUTH_ZERO_MODEL_LIVE' });
 };
 
 export const handleOwnerGoogleLogin = async (req, res, body) => {
-    return sendJSON(res, 200, { status: 'AUTH_CLEAN_ROOM_LIVE' });
+    return sendJSON(res, 200, { status: 'AUTH_ZERO_MODEL_LIVE' });
 };
