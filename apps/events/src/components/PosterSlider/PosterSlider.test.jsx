d92d43e756bb1be829cb2ad0edbc56f4882ddcc2
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-05-06 14:48:10 +0200

    Mock wind.matchMedia browser API to be able to render PosterSlider in test environment

---
+++ apps/events/src/components/PosterSlider/PosterSlider.test.jsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+import PosterSlider from './PosterSlider.Component';
+
+test('test test', () => {
+  render(<PosterSlider />);
+
+  // expect(screen.getByText(/portfolio/i)).toBeInTheDocument();
+  expect(true).toBe(false);
+});
